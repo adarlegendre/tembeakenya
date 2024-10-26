925cd67e1731d9a36584acad4ed2fb8c8dd52c5e
Zeph <adarzeph@gmail.com>
2024-10-26 23:44:54 +0200

    added connections to oracle

--- tembeakenya_app/tembeakenyasite/views.py
+++ tembeakenya_app/tembeakenyasite/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+import cx_Oracle
+
 
 def index(request):
     template = loader.get_template("tembeakenyasite/index.html")
     context = {
-        "latest_question_list": "--",
+        "sitename": "Karibu Kenya",
     }
     return HttpResponse(template.render(context, request))
+
+def connectingdb():
+    dsn_tns = cx_Oracle.makedsn('gort.fit.vutbr.cz', '1521', service_name='orclpdb')
+    connection = cx_Oracle.connect(user='xtester1', password='password', dsn=dsn_tns)
+    cursor = connection.cursor()
    
